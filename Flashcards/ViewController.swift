c184bace9a5e1578dd0788cef4d537055f8608a8
rwolde <rwolde@uw.edu>
2022-09-19 18:50:46 -0700

    added flashcards work to repo

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Reem Wolde on 9/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var frontLabel: UILabel!
+    @IBOutlet weak var backLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        frontLabel.isHidden = true
+    }
+    
+}
+
